92f1fd9762b6a7a6b1df88c58e178a44afb68a01
testfortest000 <67518721+testfortest000@users.noreply.github.com>
2020-06-29 18:02:11 +0300

    example

--- csv_analyse.py
+++ csv_analyse.py
@@ -3,7 +3,7 @@ import datetime
 now = datetime.datetime.now()
 
 
-resfile = open("otto_" + "topic-25349116_38313695" + ".csv", 'r', encoding='utf-8', errors='ignore')
+resfile = open("otto_" + "topic-25349116_24612271" + ".csv", 'r', encoding='utf-8', errors='ignore')
 links = []
 m = ['янв', 'фев', 'мар', 'апр', 'мая', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
 links_by_year = {str(b + " " + str(a)): [] for a in range(2010,2021) for b in m}
@@ -13,12 +13,17 @@ avatars = {}
 
 for line in resfile:
     data = line.split("\t")
-    links.append(data[1])
+
 
     try:
         year = data[2].split(" ")[2]
     except:
         year = str(now.year)
+
+    #if year == "2020":
+        #links.append(data[1])
+
+    links.append(data[1])
 
     try:
         month = data[2].split(" ")[1]
@@ -30,8 +35,6 @@ for line in resfile:
     except:
         avatars[data[1]] = [data[5],data[0]]
 
-
-
     links_by_year[month + " " + year].append(data[1])
 
 
@@ -42,18 +45,21 @@ html_file = open("result.html", "w", encoding="utf-8")
 html_file.write("<div style='width: 100%;'>")
 
 for cc in c:
-    print(cc)
     html_file.write("<div style='width: 19%; display:inline; text-align: center; float: left;'>"
                     "<a href='" + cc[0] + "'>" + "<p align='center'>" + avatars[cc[0]][1] + "</p>"
                     "<p align='center'>" + "<img src='" + avatars[cc[0]][0] + "' width='150'>" + "</a></p>"
                     "<p align='center'>" + str(cc[1]) + "</p>"
-                    "</div>")
+                    "</div>\n")
 
 
 html_file.write("</div>")
-for yy in range(2018,2021):
+html_file.close()
+
+
+for yy in range(2011,2021):
     for mm in m:
         if links_by_year.get(mm + " " + str(yy)):
             print(mm + " " + str(yy))
             c2 = Counter(links_by_year.get(mm + " " + str(yy))).most_common(100)
             print(c2)
+
